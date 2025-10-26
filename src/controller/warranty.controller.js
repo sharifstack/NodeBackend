872da6c6cd8b9b2e0957f889851d950e37bda5df
sharifstack <sharifahmed.sh408@gmail.com>
2025-10-26 22:14:27 +0600

    warranty controller done

--- src/controller/warranty.controller.js
+++ src/controller/warranty.controller.js
@@ -5,30 +5,108 @@ const { validateWarranty } = require("../validation/warranty.validation");
 const warrantyModel = require("../models/warranty.model");
 const productModel = require("../models/product.model");
 
-
 //create warranty
 
 exports.createWarranty = asyncHandler(async (req, res) => {
-    const warrantyData = await validateWarranty(req)
+  const warrantyData = await validateWarranty(req);
 
+  const warranty = await warrantyModel.create({ ...warrantyData });
+  if (!warranty) throw new customError(400, "warranty creation failed");
 
-    const warranty = await warrantyModel.create({ ...warrantyData })
-    if (!warranty) throw new customError(400, "warranty creation failed")
+  //pushing warranty into product
 
+  const updateProductInfo = await productModel.findOneAndUpdate(
+    {
+      _id: warrantyData.product,
+    },
+    {
+      $push: { warranty: warranty._id },
+    },
+    { new: true }
+  );
 
-    //pushing warranty into product
+  if (!updateProductInfo) throw new customError(400, "update Failed");
 
-    const updateProductInfo = await productModel.findOneAndUpdate(
-        {
-            _id: warrantyData.product
-        },
-        {
-            $push: { warranty: warranty._id }
-        },
-        { new: true })
+  apiResponse.sendsuccess(res, 201, "warranty has been created", warranty);
+});
 
-    if (!updateProductInfo) throw new customError(400, "update Failed")
+//get all warranty
+exports.getAllWarranty = asyncHandler(async (req, res) => {
+  const warranty = await warrantyModel
+    .find()
+    .populate("product")
+    .sort({ createdAt: -1 });
 
-    apiResponse.sendsuccess(res, 201, "warranty has been created", warranty)
+  if (!warranty) throw new customError(404, "warranty not found");
 
-})
+  apiResponse.sendsuccess(
+    res,
+    200,
+    "here are all warranty informations",
+    warranty
+  );
+});
+
+//get single warranty
+exports.getSingleWarranty = asyncHandler(async (req, res) => {
+  const { slug } = req.params;
+  if (!slug) throw new customError(400, "slug is required");
+
+  const warranty = await warrantyModel
+    .findOne({ slug })
+    .populate("product")
+    .sort({ createdAt: -1 });
+
+  if (!warranty) throw new customError(404, "warranty not found");
+
+  apiResponse.sendsuccess(
+    res,
+    200,
+    "warranty information  has been provided",
+    warranty
+  );
+});
+
+//update warranty
+exports.updateWarranty = asyncHandler(async (req, res) => {
+  const { slug } = req.params;
+  if (!slug) throw new customError(400, "slug is required");
+
+  const updateWarranty = await warrantyModel
+    .findOneAndUpdate({ slug }, { ...req.body }, { new: true })
+    .populate("product");
+  if (!updateWarranty) throw new customError(400, "warranty not found");
+  apiResponse.sendsuccess(
+    res,
+    200,
+    "warranty has been updated",
+    updateWarranty
+  );
+});
+
+//delete warranty
+exports.deleteWarranty = asyncHandler(async (req, res) => {
+  const { slug } = req.params;
+  if (!slug) throw new customError(400, "slug is required");
+
+  const deleteWarranty = await warrantyModel.findOneAndDelete({ slug });
+  if (!deleteWarranty) throw new customError(400, "warranty not found");
+
+  //updating product info
+  const updateProduct = await productModel.findOneAndUpdate(
+    {
+      _id: deleteWarranty.product,
+    },
+    { $pull: { warranty: deleteWarranty._id } },
+    { new: true }
+  );
+
+  if (!updateProduct)
+    throw new customError(400, "Updating The Product Info Failed");
+  apiResponse.sendsuccess(
+    res,
+    200,
+    "warranty has been deleted successfully",
+    deleteWarranty
+  );
+});
--- src/routes/api/warranty.api.js
+++ src/routes/api/warranty.api.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const _ = express.Router();
-const warrantyController = require("../../controller/warranty.controller")
+const warrantyController = require("../../controller/warranty.controller");
 
-_.route("/create-warranty").post(warrantyController.createWarranty)
-
+_.route("/create-warranty").post(warrantyController.createWarranty);
+_.route("/getall-warranty").get(warrantyController.getAllWarranty);
+_.route("/single-warranty/:slug").get(warrantyController.getSingleWarranty);
+_.route("/update-warranty/:slug").post(warrantyController.updateWarranty);
+_.route("/update-warranty/:slug").put(warrantyController.updateWarranty);
+_.route("/delete-warranty/:slug").delete(warrantyController.deleteWarranty);
 
 module.exports = _;
